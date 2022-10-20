92f100efbd934cb53ce7d522aa30d66b55d661cc
wangari <109669003+karreycodes@users.noreply.github.com>
2022-10-20 18:12:59 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -241,7 +241,7 @@ int write_pointer(char buffer[], int ind, int length,
 			buffer[1] = '0';
 			buffer[2] = 'x';
 			return (write(1, &buffer[padd_start], i - padd_start) +
-					write(1, &buffer[ind], length - (1 - padd_start) - 2));
+				write(1, &buffer[ind], length - (1 - padd_start) - 2));
 		}
 	}
 	buffer[--ind] = 'x';
