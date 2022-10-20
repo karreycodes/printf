3dd5fb440702ba3fe793274d8ce2359994488421
wangari <109669003+karreycodes@users.noreply.github.com>
2022-10-20 18:09:17 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -58,7 +58,7 @@ int handle_write_char(char c, char buffer[],
  * Return: Number of chars printed.
  */
 int write_number(int is_negative, int ind, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int length = BUFF_SIZE - ind - 1;
 	char padd = ' ', extra_ch = 0;
